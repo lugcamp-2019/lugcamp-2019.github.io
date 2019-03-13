e181beeae483ea7e85abeb2eb2f02bce39a39165
Henning Wangerin <sparklespare-login@henning.wangerin.dk>
2019-03-13 20:41:41 +0100

    < ‘v2.html’ > ‘index.html’ / ‘map.js’

--- map.js
+++ map.js
@@ -64,9 +64,6 @@ var kitchen_v = [kitchen_s[0]+offset[0],kitchen_s[1]+offset[1]];
 var kitchen_n = [kitchen_e[0]+offset[0],kitchen_e[1]+offset[1]];
 setmarker([	kitchen_e,kitchen_s,kitchen_v,kitchen_n],
 								'red',	'Professional kitchen');
-setmarker([	[54.9639617,9.6547191],	[54.9640286,9.6548037],	[54.9639510,9.6550907],	[54.96388,9.65504]	],
-								'red',	'Gym'						+'<br/>'+
-									'main computing-area'				);
 setmarker([	bib_s,			bib_v,			bib_n,			bib_e			],
 								'red',	'Former libray'					+'<br/>'+
 									'downstarts'					+'<br/>'+
@@ -76,12 +73,30 @@ setmarker([	alslug_s,		alslug_v,		alslug_n,		alslug_e		],
 									'upstarts'					+'<br/>'+
 									'meeting-room with net/power on the tables'	+'<br/>'+
 									'~20 seated people'				);
-setmarker([	[54.9639617,9.6547191],	[54.9640286,9.6548037],	[54.9640705,9.6548381],	[54.9641139,9.6546778],	[54.9640083,9.6545912]	],
-								'red',	'Shower and toilets for the gym');
+
+var osm_1283223109 = [54.9640460,9.6544625];
+var osm_6336132888 = [54.9641023,9.6545058];
+var osm_6336132887 = [54.9640918,9.6545465];
+var osm_1283223105 = [54.9641339,9.6545786];
+var osm_1283223066 = [54.9640813,9.6548059];
+
+var osm_1283223142 = [54.9640332,9.6547715];
+var osm_6336132889 = [54.9639776,9.6549932];
+var osm_1283223113 = [54.9635971,9.6551751];
+var osm_1283223207 = [54.9637946,9.6553371];
+var osm_6336132890 = [54.9639032,9.6549353];
+var osm_1283223026 = [54.9639617,9.6547191];
+var l = [osm_1283223026[0]-osm_6336132890[0],osm_1283223026[1]-osm_6336132890[1]]
+var osm_1283223026_ = [osm_1283223026[0]+l[0]*0.2,osm_1283223026[1]+l[1]*0.2];
+var osm_1283223142_ = [osm_1283223142[0]+l[0]*0.2,osm_1283223142[1]+l[1]*0.2];
+setmarker([	osm_1283223109,osm_6336132888,osm_6336132887,osm_1283223105,osm_1283223066,osm_1283223142,osm_1283223142_,osm_1283223026_	],
+								'blue',	'Toilets and shower for the gym');
+setmarker([	osm_1283223142_,osm_6336132889,osm_6336132890,osm_1283223026_	],
+								'red',	'Gym'						+'<br/>'+
+									'main computing-area'				);
+
 setmarker([	[54.96403,9.6551],	[54.96431,9.65526],	[54.96440,9.65489],	[54.96413,9.65464]	],
 								'green','Area for tents');
-var gaard_e = [54.9637946,9.6553371];
-var gaard_n = [54.9639617,9.6547191];
-setmarker([	[54.9635971,9.6551751],	gaard_e,	[(gaard_e[0]+gaard_n[0])/2,(gaard_e[1]+gaard_n[1])/2]	],
+setmarker([	osm_1283223113,osm_1283223207,osm_6336132890	],
 								'green',	'Area for caravans etc');
 osmap.fitBounds(fitBounds);
